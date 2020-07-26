162dbcec666ccb6fbd56c0c090f567b345c7c0db
Gram Stone <stonegram@gmail.com>
2020-07-25 22:40:22 -0400

    Difference procedures,  broke up random color generator, OCD
    *Added difference procedures
    *Broke up random color generator
    *OCD

--- main.rkt
+++ main.rkt
@@ -1,11 +1,12 @@
 #lang racket/base
+
 (require racket/class
          racket/gui/base
          racket/list
          racket/math
          racket/random)
 
-;;; CONSTANTS AND STRUCTURE TYPES
+;;; CONSTANTS AND STRUCTS
 
 ;constants
 (define W 800) ;canvas width
@@ -13,39 +14,41 @@
 (define w 2)   ;pen width
 (define n 32)  ;number of sites and colors
 
-;structure type definitions
+;structs
 (struct point (x y))
 (struct site (pos color))
 
 ;;; MATH
 
 ;custom Cartesian product procedure (only computes binary Cartesian products and outputs a pair? of any/c instead of a list? to slightly abbreviate our definition of the Euclidean plane)
-(define (cartesian-product lst1 lst2)(apply append (for/list ([i lst1])
-                                                     (for/list ([j lst2])
-                                                       (cons i j)))))
+(define (cartesian-product lst1 lst2)(apply append
+                                            (for/list ([i lst1])
+                                              (for/list ([j lst2])
+                                                (cons i j)))))
 
 ;Euclidean plane definition
 (define plane (map (λ (x) (point (car x) (cdr x)))
                    (cartesian-product (range 0 W)
                                       (range 0 H))))
 
+;difference procedures
+(define (dx a b)(- (point-x a)
+                   (point-x b)))
+
+(define (dy a b)(- (point-y a)
+                   (point-y b)))
+
 ;Euclidean distance procedure
-(define (euclidean-distance a b)(sqrt (+ (sqr (- (point-x b)
-                                                 (point-x a)))
-                                         (sqr (- (point-y b)
-                                                 (point-y a))))))
+(define (euclidean-distance a b)(sqrt (+ (sqr (dx a b))
+                                         (sqr (dy a b)))))
 
 ;Manhattan distance procedure
-(define (manhattan-distance a b)(+ (abs (- (point-x a)
-                                           (point-x b)))
-                                   (abs (- (point-y a)
-                                           (point-y b)))))
+(define (manhattan-distance a b)(+ (abs (dx a b))
+                                   (abs (dy a b))))
 
 ;Chebyshev distance procedure
-(define (chebyshev-distance a b)(max (abs (- (point-x a)
-                                             (point-x b)))
-                                     (abs (- (point-y a)
-                                             (point-y b)))))
+(define (chebyshev-distance a b)(max (abs (dx a b))
+                                     (abs (dy a b))))
 
 ;distance procedure alias (use it to toggle distance functions)
 (define distance euclidean-distance)
@@ -55,20 +58,30 @@
 
 ;;; RANDOM SITE GENERATOR
 
+;random color procedure
+(define (random-color)(make-object color% (random 0 256) (random 0 256) (random 0 256)))
+
 ;defines a list of random colors with length n
 (define colors (for/list ([i (range n)])
-                 (make-object color% (random 0 256) (random 0 256) (random 0 256))))
+                 (random-color)))
+
+;defines a list of random points with length n
+(define points (random-sample plane n #:replacement? #f))
 
 ;defines a list of random sites with length n
-(define sites (map site (random-sample plane n #:replacement? #f) colors))
+(define sites (map site points colors))
 
 ;;;GRAPHICS
 
-(define frame (new frame%
-                   [label "Random Voronoi Diagram Generator"]
-                   [width W]
-                   [height H]))
-(new canvas% [parent frame]
+;top level frame
+(define f (new frame%
+               [label "Random Voronoi Diagram Generator"]
+               [width W]
+               [height H]))
+
+;canvas
+(new canvas%
+     [parent f]
      [paint-callback
       (λ (canvas dc)
         (define (draw p)
@@ -76,4 +89,5 @@
             (set-pen (send the-pen-list find-or-create-pen (site-color (nearest-neighbor p sites)) w 'solid))
             (draw-point (point-x p) (point-y p))))
         (for-each draw plane))])
-(send frame show #t)
+
+(send f show #t)
